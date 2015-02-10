e2c235cae63d354b82616f4e8f78c572577f289a
PeterisFridvalds <peterisf@inbox.lv>
2015-02-10 13:18:19 +0200

    2015.10.02.-13:20
    nestrādā datu izvade no tabulas

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls import patterns, url
 from blog import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+        url(r'^$', views.index, name='index'),
+        url(r'^add_post/$', views.add_post, name='add_post'),
+                       )
--- blog/views.py
+++ blog/views.py
@@ -2,17 +2,21 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post
 
 def index(request):
-    # Request the context of the request.
-    # The context contains information such as the client's machine details, for example.
+    # Request the context of HTTP request
     context = RequestContext(request)
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key content is the same as {{ content }} in the template!
-    context_dict = {'content': "I am bold font from the context"}
+    post_list = post.objects.order_by('-date')[:5]
+    context_dict = {'posts': post_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render_to_response('index.html', context_dict, context)
+
+def add_post(request):
+    return render_to_response('add_post.html')
